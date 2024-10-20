bacb940f992bf7c25e1386cb46289bc7265dcf6e
harry Nguyen <hainguyen27798@gmail.com>
2024-10-20 18:22:06 +0700

    config eslint and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzWaveModule } from 'ng-zorro-antd/core/wave';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzButtonComponent, NzWaveModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 import type { ApplicationConfig } from '@angular/core';
+import { importProvidersFrom } from '@angular/core';
 import { provideZoneChangeDetection } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 
 import { routes } from './app.routes';
 
@@ -8,5 +14,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideClientHydration(),
+    provideNzI18n(en_US),
+    importProvidersFrom([CommonModule, FormsModule]),
+    provideHttpClient(),
   ],
 };
